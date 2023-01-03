4f2da5a4f46cf41cc3b4e82bb6270dd2997b99bb
Julien Charbonneau <juliencharbonneauqc@gmail.com>
2023-01-03 11:11:26 -0500

    add validation to email input

--- component/Login.js
+++ component/Login.js
@@ -1,20 +1,27 @@
 import React, { useState } from 'react';
 import { View, TextInput, Button } from 'react-native';
+import { isEmail } from 'validator';
+
 
 const Login = () => {
-  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const handleLogin = () => {
-    // send a request to the server to verify the username and password
+    if (!isEmail(email)) {
+      alert("Not a valid email.")
+      return;
+    }
+  
+    // the email is valid, send a request to the server to verify the email and password
   };
 
   return (
     <View>
       <TextInput
-        value={username}
-        onChangeText={setUsername}
-        placeholder="Username"
+        value={email}
+        onChangeText={setEmail}
+        placeholder="Email"
       />
       <TextInput
         value={password}
