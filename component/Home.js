4ae72dbc52c961b6eb3de259675887a3e1ba7c24
Julien Charbonneau <juliencharbonneauqc@gmail.com>
2023-01-04 12:58:44 -0500

    display elevator not in operation in the home page

--- component/Home.js
+++ component/Home.js
@@ -1,10 +1,41 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList } from 'react-native';
+import axios from 'axios';
 
 const Home = () => {
+  const [elevatorStatusList, setElevatorStatusList] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // send a GET request to the API endpoint to get a list of elevator statuses
+        const response = await axios.get(
+          `https://5863-209-226-0-76.ngrok.io/api/Elevator/GetAllElevatorStatusNotOperation`
+        );
+        console.log("-----------test------------", response.data);
+        setElevatorStatusList(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <View>
-      <Text>Welcome to the home page!</Text>
+    <View style={{ flex: 1, justifyContent: 'center' }}>
+      <Text style={{ fontSize: 20, textAlign: 'center' }}>Elevators not in operation</Text>
+      <FlatList
+        data={elevatorStatusList}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={{ alignItems: 'center' }}>
+            <Text style={{ fontSize: 16, textAlign: 'center' }}>
+              {`Elevator id: ${item.id} status: ${item.status}`}
+            </Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
