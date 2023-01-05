ca34be1fa7b2a87072f6afbb53b16a8a68d6d41c
Julien Charbonneau <juliencharbonneauqc@gmail.com>
2023-01-05 10:35:41 -0500

    change list element by button and only display the elevator id

--- component/Home.js
+++ component/Home.js
@@ -4,6 +4,25 @@ import axios from 'axios';
 import { useNavigation } from '@react-navigation/native';
 
 
+
+// Button
+const onElevatorPress = (elevator) => {
+  console.log("onElevatorPress elevator:", elevator);
+}
+const Item = ({ elevator }) => {
+  console.log("id is:", elevator);
+
+  return (
+    <Button
+      title={elevator.id.toString()}
+      onPress={() => onElevatorPress(elevator)}
+    />
+
+  );
+};
+// button end
+
+
 const Home = () => {
   const navigation = useNavigation();
   const [elevatorStatusList, setElevatorStatusList] = useState([]);
@@ -13,7 +32,7 @@ const Home = () => {
       try {
         // send a GET request to the API endpoint to get a list of elevator statuses
         const response = await axios.get(
-          `https://be27-209-226-0-76.ngrok.io/api/Elevator/GetAllElevatorStatusNotOperation`
+          `https://59ef-209-226-0-76.ngrok.io/api/Elevator/GetAllElevatorStatusNotOperation`
         );
         setElevatorStatusList(response.data);
       } catch (error) {
@@ -24,21 +43,17 @@ const Home = () => {
     fetchData();
   }, []);
 
+  const renderItem = ({ item }) => (
+    <Item elevator={item} />
+  );
+
   return (
     <View style={{ flex: 1, justifyContent: 'center' }}>
       <Text style={{ fontSize: 20, textAlign: 'center' }}>Elevators not in operation</Text>
       <FlatList
         data={elevatorStatusList}
+        renderItem={renderItem}
         keyExtractor={item => item.id.toString()}
-        renderItem={({ item }) => ( // item represent an elevator
-          <View style={{ alignItems: 'center' }}>
-            <Text style={{ fontSize: 16, textAlign: 'center' }}>
-              {`Elevator id: ${item.id} status: ${item.status}`}
-            </Text>
-
-         
-          </View>
-        )}
       />
          <View style={{ flex: 1, justifyContent: 'flex-end' }}>
          <Button
